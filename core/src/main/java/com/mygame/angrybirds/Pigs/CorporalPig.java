bb44541a9423730881b5a96e3cbbf75e63603abd
Pranshu <pranshu23385@iiitd.ac.in>
2024-11-21 10:48:59 +0530

    Version 3.8 all required pig class made

--- core/src/main/java/com/mygame/angrybirds/Pigs/CorporalPig.java
+++ core/src/main/java/com/mygame/angrybirds/Pigs/CorporalPig.java
@@ -1,4 +1,48 @@
 package com.mygame.angrybirds.Pigs;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 public class CorporalPig {
+    private Texture texture;
+    private int health;
+    private float x, y;
+
+    public CorporalPig(float x, float y) {
+        this.texture = new Texture(Gdx.files.internal("ch/MinionPig.png"));
+        this.health = 100;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, x, y, texture.getWidth() * 0.08f, texture.getHeight() * 0.08f);
+    }
+
+    public boolean isDestroyed() {
+        return health <= 0;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, texture.getWidth() * 0.08f, texture.getHeight() * 0.08f);
+
+    }
 }
